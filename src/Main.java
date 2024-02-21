e1cbb76d1f21038907bbb15fede2c91196c7c60c
KrisK <37834268+KristofferKay@users.noreply.github.com>
2024-02-21 11:44:23 +0100

    user story 2 complete

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,51 @@ public class Main {
 
     public static void main(String[] args) {
 
+        final int SENTINEL = 2;
+        int loopInput = 0;
+
         // initiate scanner for main
         Scanner scanner = new Scanner(System.in);
 
         // obj for Movie Collection
         MovieCollection addMovie = new MovieCollection();
 
-        // user input
-        System.out.println("Hvad er navnet på filmen?");
-        String movie = scanner.nextLine();
+        while (loopInput != SENTINEL) {
+            System.out.println("Velkommen til din filmsamling");
+            System.out.println("1. Opret film");
+            System.out.println("2. Afslut");
+            loopInput = scanner.nextInt();
 
-        System.out.println("Hvem har skrevet filmen");
-        String director = scanner.nextLine();
+            if (loopInput == 1 ) {
 
-        System.out.println("Er filmen i farver");
-        boolean isInColor = scanner.nextBoolean();
+                 // user input
+                 System.out.println("Hvad er navnet på filmen?");
+                 String movie = scanner.next();
 
-        System.out.println("Hvor lang er filmen i minutter");
-        int lengthInMinutes = scanner.nextInt();
+                 System.out.println("Hvem har skrevet filmen");
+                 String director = scanner.next();
 
-        System.out.println("Hvad er genre");
-        String genre = scanner.next();
+                 System.out.println("Er filmen i farver");
+                 boolean isInColor = scanner.nextBoolean();
 
-        System.out.println("Hvilen år er filmen lavet");
-        int yearsCreated = scanner.nextInt();
+                 System.out.println("Hvor lang er filmen i minutter");
+                 int lengthInMinutes = scanner.nextInt();
 
-        addMovie.addToMovieCollection(movie, director, isInColor, lengthInMinutes, genre, yearsCreated);
+                 System.out.println("Hvad er genre");
+                 String genre = scanner.next();
+
+                 System.out.println("Hvilen år er filmen lavet");
+                 int yearsCreated = scanner.nextInt();
+
+                 addMovie.addToMovieCollection(movie, director, isInColor, lengthInMinutes, genre, yearsCreated);
+
+                 System.out.println("Du har tilføjet " + movie + " til din samling");
+             }
+        }
+        if (loopInput == SENTINEL) {
+            System.out.println("Du har afsluttet processen");
+        }
+
 
     }
 }
